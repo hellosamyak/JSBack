3fbbaa29d8fcae0404b875d5f3dba0be4ee2bae9
hellosamyak <jainsamyak0805@gmail.com>
2026-07-25 22:18:23 +0530

    add missing routes for tweets, subscriptions, videos, comments, and likes

--- project/src/app.js
+++ project/src/app.js
@@ -1,6 +1,6 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express()
 
@@ -23,9 +23,21 @@ app.use(cookieParser())
 
 // routes
 import userRouter from "./routes/user.route.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
 
 // example url
 // http://localhost:8000/api/v1/users/register
