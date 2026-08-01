f699d11a5726a64b36dc237939647c6bfe2a2156
hellosamyak <jainsamyak0805@gmail.com>
2026-08-01 14:50:31 +0530

    fix: update dotenv import and configuration for environment variables

--- project/src/index.js
+++ project/src/index.js
@@ -1,6 +1,10 @@
-import "dotenv/config"
+import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 import { app } from "./app.js"
+
+dotenv.config({
+    path: "./env"
+})
 
 const port = process.env.PORT || 8000
 
